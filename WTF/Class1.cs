24c53aa5a9c99d299cf6a166a3ee2dd830c9d018
Juan León <126162340+Depechejuan@users.noreply.github.com>
2024-12-20 19:16:46 +0100

    update

---
+++ WTF/Class1.cs
@@ -0,0 +1,38 @@
+﻿namespace WTF
+{
+    public class Class1
+    {
+        public static void Shift(char[] arr, int desplazamiento)
+        {
+            if (arr == null || arr.Length == 0)
+                return;
+            int mov = desplazamiento;
+            if (desplazamiento >= arr.Length)
+                mov = desplazamiento % arr.Length;
+
+            char aux = arr[arr.Length-1];
+            int fate = arr.Length - 1;
+
+            for (int i = 0; i < arr.Length -1; i++)
+            {
+                int position = fate - mov;
+                if (position < 0)
+                    position += arr.Length;
+                if (fate < 0)
+                    fate += arr.Length;
+                arr[position] = arr[fate];
+                fate -= mov;
+                position -= mov;
+
+                if (i == 0)
+                    arr[fate] = aux;
+            }
+        }
+
+        public static void PrintArr(char[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine(arr[i]);
+        }
+    }
+}
---
+++ WTF/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace WTF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = new char[5];
+            arr[0] = 'S';
+            arr[1] = 'O';
+            arr[2] = 'F';
+            arr[3] = 'I';
+            arr[4] = 'A';
+
+            Class1.Shift(arr, 2);
+            Class1.PrintArr(arr);
+
+        }
+    }
+}
